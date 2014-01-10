524d074f5357e0b4b765edf71e15d36e0bfaf335
Warren Buckley <warren@creativewebspecialist.co.uk>
2014-01-10 16:22:03 +0000

    Add Search Packages to resource JS file

--- UmbracoNuget/UmbracoNuget.Site/App_Plugins/NuGet/BackOffice/Packages/resources/NuGet.Resource.js
+++ UmbracoNuget/UmbracoNuget.Site/App_Plugins/NuGet/BackOffice/Packages/resources/NuGet.Resource.js
@@ -4,8 +4,17 @@
             getPackages: function (sortBy) {
                 return $http.get("NuGet/PackageApi/GetPackages?sortBy=" + sortBy);
             },
+
             getPackagePage: function (sortBy, pageNumber) {
                 return $http.get("NuGet/PackageApi/GetPackages?sortBy=" + sortBy + "&page=" + pageNumber);
-            }
+            },
+
+            searchPackages: function (searchTerm, pageNumber) {
+                return $http.get("NuGet/PackageApi/SearchPackages?searchTerm=" + searchTerm);
+            },
+
+            searchPackagesPage: function (searchTerm, pageNumber) {
+                return $http.get("NuGet/PackageApi/SearchPackages?searchTerm=" + searchTerm + "&page=" + pageNumber);
+        }
         };
     });
