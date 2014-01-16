7bfeab1d2848c834513a024523ee379200c43adc
warrenbuckley <warren@creativewebspecialist.co.uk>
2014-01-16 13:02:20 +0000

    Some view refactoring. Have a PartialController so depending on method name in routeParams AngularJS object dpeneds on partial route to html file

--- UmbracoNuget/UmbracoNuget.NightlySite/App_Plugins/NuGet/BackOffice/Packages/controllers/Edit.Controller.js
+++ UmbracoNuget/UmbracoNuget.NightlySite/App_Plugins/NuGet/BackOffice/Packages/controllers/Edit.Controller.js
@@ -4,7 +4,9 @@
         //Currently loading /umbraco/general.html
         //Need it to look at /App_Plugins/
         var viewName    = $routeParams.id;
-        viewName        = viewName.replace('%20', '-').replace(' ', '-');
+        viewName = viewName.replace('%20', '-').replace(' ', '-');
+
+        console.log($routeParams);
 
         $scope.templatePartialURL   = '../App_Plugins/NuGet/backoffice/Packages/partials/edit/' + viewName + '.html';
     });
